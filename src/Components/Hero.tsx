0b6cdab6a3c8256c0a91cfc9d089deae9d710fdf
unknown <giri@10000>
2023-03-26 16:37:02 +0300

    added something

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { useDarkMode } from '../context/context';
 
 function Hero() {
+    const { darkMode ,setDarkMode } = useDarkMode();
     return (
         <div className='-mt-14 relative'>
-            <div className='absolute top-0 left-0 w-full h-full bg-gradient-to-t from-zinc-900 to-transparent z-0'></div>
-            <img className='h-[70vh] w-full object-center	object-cover z-0' src='./1524018.webp' alt='background'></img>
+            <div className={`absolute top-0 left-0 w-full h-full ${darkMode === true? 'bg-gradient-to-t from-zinc-900 to-transparent z-0' :'bg-gradient-to-t from-transparent/10 to-transparent/5 z-0'}`}></div>
+            <img className='h-[70vh] w-full object-center	object-cover z-0' src='./Palmen.jpg' alt='background'></img>
         </div>
     );
 }
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -20,8 +20,13 @@ function Navbar() {
   }
 
   return (
-    <div className='h-14 flex justify-between px-5 md:px-20 py-5'>
-      <div className='logo' >logo</div>
+    <div className={`h-20 flex justify-between px-5 md:px-20 py-5  ${darkMode === true? 'bg-zinc-800 shadow-lg shadow-zinc-900/50 ' :'bg-zinc-100 shadow-lg shadow-white/50'}`}>
+      <div className='logo w-64 h-64 -mt-16'>
+        {darkMode
+          ? <img src='./joshco1.png' alt='Dark logo' />
+          : <img src='./joshco2.png' alt='Light logo' />
+        }
+      </div>
       <div className='menus flex space-x-5 items-center ' >
         <div
           className='hidden md:block relative inline-block w-10 h-6 py-1 px-1  rounded-full overflow-hidden bg-blue-600 dark:bg-blue-600 cursor-pointer'
@@ -42,21 +47,21 @@ function Navbar() {
         </div>
         <div className="block md:hidden">
           <button className=' px-4 py-1 rounded-lg text-sm ' onClick={handleMenuToggle}>
-          <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
-          {menuOpen ? (
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M3.75 3.75H20.25V5.25H3.75V3.75ZM3.75 9.75H20.25V11.25H3.75V9.75ZM20.25 18.75H3.75V17.25H20.25V18.75Z"
-            />
-          ) : (
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M4.5 6.75H19.5V8.25H4.5V6.75ZM4.5 11.25H19.5V12.75H4.5V11.25ZM4.5 15.75H19.5V17.25H4.5V15.75Z"
-            />
-          )}
-        </svg>
+            <svg viewBox="0 0 24 24" className="h-6 w-6 fill-current">
+              {menuOpen ? (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M3.75 3.75H20.25V5.25H3.75V3.75ZM3.75 9.75H20.25V11.25H3.75V9.75ZM20.25 18.75H3.75V17.25H20.25V18.75Z"
+                />
+              ) : (
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M4.5 6.75H19.5V8.25H4.5V6.75ZM4.5 11.25H19.5V12.75H4.5V11.25ZM4.5 15.75H19.5V17.25H4.5V15.75Z"
+                />
+              )}
+            </svg>
           </button>
         </div>
       </div>
@@ -79,21 +84,21 @@ function Navbar() {
               </button>
             </div>
             <div className='flex justify-between' >
-            <div
-              className='relative inline-block w-10 h-6 py-1 px-1  rounded-full overflow-hidden bg-blue-600 dark:bg-blue-600 cursor-pointer'
-              onClick={handleDarkModeToggle}
-            >
               <div
-                className={`absolute w-4 h-4 bg-gray-700 rounded-full shadow-md transform transition-transform duration-300 ease-in-out ${darkMode ? 'translate-x-4' : ''
-                  }`}
+                className='relative inline-block w-10 h-6 py-1 px-1  rounded-full overflow-hidden bg-blue-600 dark:bg-blue-600 cursor-pointer'
+                onClick={handleDarkModeToggle}
               >
+                <div
+                  className={`absolute w-4 h-4 bg-gray-700 rounded-full shadow-md transform transition-transform duration-300 ease-in-out ${darkMode ? 'translate-x-4' : ''
+                    }`}
+                >
+                </div>
+
               </div>
+              <div>
+                <img src='./flag.png' className='w-9 h-6 object-contain ' style={{ zIndex: -1 }} alt='flag' />
 
-            </div>
-            <div>
-              <img src='./flag.png' className='w-9 h-6 object-contain ' style={{zIndex:-1}} alt='flag' />
-
-            </div>
+              </div>
             </div>
           </div>
 
